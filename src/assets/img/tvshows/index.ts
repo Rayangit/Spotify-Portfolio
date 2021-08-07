70bebf1e34aa7b2ec18a1af7bff171f18e713549
Rakedd <Rayan.keddache@epitech.eu>
2021-08-07 04:27:01 +0200

    [Images] Fix url

--- src/assets/img/tvshows/index.ts
+++ src/assets/img/tvshows/index.ts
@@ -2,9 +2,9 @@ const s3BucketURL = 'https://d2boeulkbylv6v.cloudfront.net/img/'
 
 const LaCasaDePapel = s3BucketURL + 'la-casa-de-papel.jpg'
 const MrRobot = s3BucketURL + 'mr-robot.jpg'
-import BreakingBad from './breakingBad.jpg'
-import Brooklyn99 from './brooklyn99.jpg'
-import Got from './got.jpg'
+const BreakingBad = s3BucketURL + 'breakingBad.jpg'
+const Brooklyn99 = s3BucketURL + 'brooklyn99.jpg'
+const Got = s3BucketURL + 'got.jpg'
 const TheLastKingdom =  s3BucketURL + 'the-last-kingdom.jpg'
 
 export {
